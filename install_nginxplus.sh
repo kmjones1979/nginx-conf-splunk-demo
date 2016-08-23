a2160507217c1a7430e8789aec0b2c6c370f9635
Kevin Jones <jones.kevin@gmail.com>
2016-08-23 13:42:39 -0700

    adding small changes

--- install_nginxplus.sh
+++ install_nginxplus.sh
@@ -2,33 +2,33 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 echo -e "${GREEN}# mkdir -p /etc/ssl/nginx${NC}"
-sleep 2
+sleep 3
 mkdir -p /etc/ssl/nginx
 
 echo -e "${GREEN}# cp nginx-repo.* /etc/ssl/nginx/${NC}"
-sleep 2
+sleep 3
 cp nginx-repo.* /etc/ssl/nginx/
 
 echo -e "${GREEN}# yum install -y ca-certificates vim${NC}"
-sleep 2
+sleep 3
 yum install -y ca-certificates vim
 
 echo -e "${GREEN}# wget -P /etc/yum.repos.d https://cs.nginx.com/static/files/nginx-plus-7.repo${NC}"
-sleep 2
+sleep 3
 wget -P /etc/yum.repos.d https://cs.nginx.com/static/files/nginx-plus-7.repo
 
 echo -e "${GREEN}# yum install -y nginx-plus${NC}"
-sleep 2
+sleep 3
 yum install -y nginx-plus
 
 echo -e "${GREEN}# /usr/sbin/nginx${NC}"
-sleep 2
+sleep 3
 /usr/sbin/nginx
 
 echo -e "${GREEN}# ps aux | grep nginx${NC}"
-sleep 2
+sleep 3
 ps aux | grep nginx
 
 echo -e "${GREEN}# curl localhost${NC}"
-sleep 2
+sleep 3
 curl localhost
--- install_splunk.sh
+++ install_splunk.sh
@@ -2,25 +2,29 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 echo -e "${GREEN}# yum install -y vim${NC}"
-sleep 2
+sleep 3
 yum install -y vim
 
 echo -e "${GREEN}# wget https://download.splunk.com/products/splunk/releases/6.4.2/linux/splunk-6.4.2-00f5bb3fa822-linux-2.6-x86_64.rpm${NC}"
-sleep 2
+sleep 3
 wget https://download.splunk.com/products/splunk/releases/6.4.2/linux/splunk-6.4.2-00f5bb3fa822-linux-2.6-x86_64.rpm
 
 echo -e "${GREEN}# rpm -i splunk-6.4.2-00f5bb3fa822-linux-2.6-x86_64.rpm${NC}"
-sleep 2
+sleep 3
 rpm -i splunk-6.4.2-00f5bb3fa822-linux-2.6-x86_64.rpm
 
 echo -e "${GREEN}# /opt/splunk/bin/splunk start --accept-license${NC}"
-sleep 2
+sleep 3
 /opt/splunk/bin/splunk start --accept-license
 
 echo -e "${GREEN}# /opt/splunk/bin/splunk enable boot-start${NC}"
-sleep 2
+sleep 3
 /opt/splunk/bin/splunk enable boot-start
 
+echo -e "${GREEN}# ps aux | grep splunk${NC}"
+sleep 3
+ps aux | grep splunk
+
 echo -e "${GREEN}# curl -ILs localhost:8000 | grep HTTP${NC}"
-sleep 2
+sleep 3
 curl -ILs localhost:8000 | grep HTTP
