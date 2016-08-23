3ffcdd2eb5096eb739b635b4f00853587cb80982
Kevin Jones <jones.kevin@gmail.com>
2016-08-23 13:03:20 -0700

    adding color changes

--- install_splunkforwarder.sh
+++ install_splunkforwarder.sh
@@ -1,22 +1,22 @@
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-echo "# wget https://download.splunk.com/products/universalforwarder/releases/6.4.2/linux/splunkforwarder-6.4.2-00f5bb3fa822-linux-2.6-x86_64.rpm"
+echo -e "${GREEN}# wget https://download.splunk.com/products/universalforwarder/releases/6.4.2/linux/splunkforwarder-6.4.2-00f5bb3fa822-linux-2.6-x86_64.rpm${NC}"
 sleep 2
 wget https://download.splunk.com/products/universalforwarder/releases/6.4.2/linux/splunkforwarder-6.4.2-00f5bb3fa822-linux-2.6-x86_64.rpm
 
-echo "# rpm -i splunkforwarder-6.4.2-00f5bb3fa822-linux-2.6-x86_64.rpm"
+echo -e "${GREEN}# rpm -i splunkforwarder-6.4.2-00f5bb3fa822-linux-2.6-x86_64.rpm${NC}"
 sleep 2
 rpm -i splunkforwarder-6.4.2-00f5bb3fa822-linux-2.6-x86_64.rpm
 
-echo "# /opt/splunkforwarder/bin/splunk start --accept-license"
+echo -e "${GREEN}# /opt/splunkforwarder/bin/splunk start --accept-license${NC}"
 sleep 2
 /opt/splunkforwarder/bin/splunk start --accept-license
 
-echo "# /opt/splunkforwarder/bin/splunk enable boot-start"
+echo -e "${GREEN}# /opt/splunkforwarder/bin/splunk enable boot-start${NC}"
 sleep 2
 /opt/splunkforwarder/bin/splunk enable boot-start
 
-echo "# ps aux | grep splunk"
+echo -e "${GREEN}# ps aux | grep splunk${NC}"
 sleep 2
 ps aux | grep splunk
